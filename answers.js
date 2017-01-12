a1991cf0502da28b69290bd29015bffeb5858106
Allen <allen.decodemtl@gmail.com>
2017-01-12 20:57:00 +0000

    Exercise 1 to 5

--- answers.js
+++ answers.js
@@ -2,28 +2,71 @@
 
 var arr1 = [2,-1,5,-3,9];
 
-arr1.forEach(function(element){
+function printPositives(element){
     if (element >0){
         console.log(element);
     }
-});
+}
+
+arr1.forEach(printPositives);
+
 
 
 // Similar to the previous exercise, write a function called getPositives that takes an array and uses the filter method to return a new array with only the positive numbers.
-var arr2 = arr1.filter(function(element){
+
+function getPositives(element){
     return element > 0
-});
+}
+
+var arr2 = arr1.filter(getPositives);
 
 console.log(arr2);
 
 
+
 // Re-do exercise 1 by first filtering the input array, and then printing the numbers from the filtered array. Your code will look something like: return arr.filter(...).forEach(...).
-[2,-1,5,-3,9].filter(function(element){
-    return element > 0
-}).forEach(function(element){
-    console.log(element)
-});
+arr1.filter(getPositives).forEach(printPositives);
+
 
 
 // Write a function called filterArray that takes a callback function and an array as arguments. 
 // Your filterArray function should return a new array that contains only the elements where the callback function returns true.
+// NOTE: This is a trick question. The answer is a one-liner and you are allowed to use array.filter :)
+function filterArray(arr, cb){
+    return arr.filter(cb)
+};
+
+var arr3 = filterArray(arr1, function(element){ return element > 2 });
+
+console.log(arr3);
+
+
+
+// Write a function called longestWord that takes a string as argument, and returns the longest word in the string. 
+// You should use Array.prototype.reduce to do your work.
+// Hint: You can use String.prototype.split to split the string into an array of words.
+
+function longestWord(str) {
+  // Step 1. Split the string into an array of strings
+  var strSplit = str.split(' ');
+  
+  // Step 2. Use the reduce method
+  var longestWord = strSplit.reduce(function(accumulator, currentValueBeingEvaluated) {
+    if(currentValueBeingEvaluated.length > accumulator.length)
+       return currentValueBeingEvaluated;
+    else
+       return accumulator;
+  }, "");
+  
+  // Step 3. Return the length of the longestWord
+  return longestWord;
+}
+
+console.log(longestWord("I am studying at a bootcamp"));
+
+
+// Write a function called countVowels that takes a string and returns the number of vowels in the string. You should use Array.prototype.reduce to do your work.
+// For the string "The quick brown fox", the output should be 5 because there is one e, one u, one i and two os.
+// Hint: You can use String.prototype.split again. There is a way to use it to split a string by character. Try to Google it :)
+// Hint 2: You can create an array of vowels and use Array.prototype.indexOf to check if the current letter is a vowel.
+
